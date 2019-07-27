b1737eea8f2ccd7675b01d6835f32bbd8b27a3f3
Jason Yan <bluewatercube2@gmail.com>
2019-07-27 18:01:37 -0400

    Delete testImages code
    updated checkSpaces to delete testImage photos

--- webserver/checkSpaces.py
+++ webserver/checkSpaces.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import numpy as np
+import shutil
+
 
 '''
 checkSpaces will produce segment folder with pictures of cars
@@ -21,6 +23,14 @@ def checkSpaces(rawimage):
     # save segmented pictures
     if not os.path.exists("./segment"):
         os.mkdir("./segment")
+
+    dirPath = '../segmentData/'
+    # Delete all contents of a directory using shutil.rmtree() and handle exceptions
+    try:
+        shutil.rmtree(dirPath)
+    except:
+        print('Error while deleting directory')
+
     for y in range(len(keys_vert)-1):
         for x in range(0, len(keys_horizontal)-1, 2):
             cv2.imwrite("./segment/{:04d}_{:04d}".format(int(x/2), int(y)) + '.jpg', \
